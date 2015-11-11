0af1f16885d741b3cc6465a1aae2fb96540a96bb
Sergey <sergio_lebedev@yahoo.it>
2015-11-11 15:36:15 +0100

    working exercise

--- 02-valentine/dev-code/app.js
+++ 02-valentine/dev-code/app.js
@@ -1,8 +1,34 @@
 /* your code should go here */
 
+var tmpl =  '<li>' +
+                '<img src="img/themeNUMBER.jpg">' +
+                '<h2>TITLE</h2>' +
+                '<button class="opt-choose" value="themeNUMBER">Choose</button>' +
+            '</li>';
+var picture = '<img src="img/VALUE.jpg">';
+
+
 $(document).ready(function(){
-  
-  
+    var i;
+    for(i = 0; i < data.length; i++){
+        $(".themes").append(tmpl.replace(/NUMBER/g, i+1).replace("TITLE", data[i].name));
+    }
+    
+    $(".opt-choose").click( function () {
+        
+        // check for proper input
+        if( $("#inputFrom").val() == "" || $("#inputTo").val() == "" || $("#inputMsg").val() == ""){
+            alert("Please enter all fields");
+        } else { // inputs are correctly filled
+            
+            $(".picture").html( picture.replace("VALUE", this.value) );
+            $("#cardTo").html( $("#inputTo").val() );
+            $("#cardMsg").html( $("#inputMsg").val() );
+            $("#cardFrom").html( $("#inputFrom").val() );
+            $("#preview").show();
+        }
+    });
+    
 });
 
 
